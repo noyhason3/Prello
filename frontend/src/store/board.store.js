0d17fc728b8b4a23f1d69b545e164fde067521fc
noyhason3 <noyhason3@gmail.com>
2021-03-17 13:26:43 +0200

    pulling

--- frontend/src/store/board.store.js
+++ frontend/src/store/board.store.js
@@ -1,42 +1,55 @@
 export const boardStore = {
-    state: {
-        board: null,
+  state: {
+    board: null,
+    boardList:null
+  },
+  mutations: {
+    setBoard(state, { savedBoard }) {
+      state.board = savedBoard;
     },
-    mutations: {
-        setBoard(state, { savedBoard }) {
-            state.board = savedBoard;
-        },
+    setBoardList(state, {boards}){
+        state.boards = boards;
+    }
+  },
+  actions: {
+    async loadBoardList({ commit }) {
+      try {
+        const boards =  await boardService.query()
+        commit({type:'setBoardList', boards})
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
-    actions: {
-        async loadBoard({ commit }) {
-            try {
-                const board = await boardService.query();
-                commit({ type: 'setBoard', board });
+    async loadBoard({ commit }) {
+      try {
+        const board = await boardService.query();
+        commit({ type: 'setBoard', board });
 
-                // socketService.off(SOCKET_EVENT_BOARD_UPDATE);
-                // socketService.emit('joinBoard', board._id);
-                // socketService.on(SOCKET_EVENT_BOARD_UPDATE, (savedBoard) => {
-                //     context.commit({ type: 'setBoard', savedBoard });
-                // });
-                // socketService.on(SOCKET_EVENT_TASK_UPDATE, (task) => {
-                //     context.commit({ type: 'updateTask', task });
-                // });
-            } catch (err) {
-                console.log(err);
-                throw err;
-            }
-        },
-        async saveBoard({ commit }, board) {
-            try {
-                await socketService.emit('saveBoard', board);
-
-                const savedBoard = await boardService.save(board);
-                commit({ type: 'setBoard', savedBoard });
-            } catch (err) {
-                console.log(err);
-                throw err;
-            }
-        },
+        socketService.off(SOCKET_EVENT_BOARD_UPDATE);
+        socketService.emit('joinBoard', board._id);
+        socketService.on(SOCKET_EVENT_BOARD_UPDATE, (savedBoard) => {
+          context.commit({ type: 'setBoard', savedBoard });
+        });
+        socketService.on(SOCKET_EVENT_TASK_UPDATE, (task) => {
+          context.commit({ type: 'updateTask', task });
+        });
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
-    modules: {},
+    async saveBoard({ commit }, board) {
+      try {
+        await socketService.emit('saveBoard', board);
+
+        const savedBoard = await boardService.save(board);
+        commit({ type: 'setBoard', savedBoard });
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+  },
+  modules: {},
 };
