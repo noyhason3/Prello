21f9a375b303718c402e93d32ea9c440a4844bd5
noyhason3 <noyhason3@gmail.com>
2021-03-18 00:35:37 +0200

    pulling

--- frontend/src/cmps/task/task-cmps/task-checklist.vue
+++ frontend/src/cmps/task/task-cmps/task-checklist.vue
@@ -1,11 +1,23 @@
 <template>
   <section class="task-checklist">
     <ul>
-      <li v-for="checklist in task.checklists" :key="checklist.id" class="clean-list">
-        <header
-          >✅ {{ checklist.title }} <button>Delete</button>
-        </header>
-        <input type="text" />
+      <li
+        v-for="checklist in task.checklists"
+        :key="checklist.id"
+        class="clean-list"
+      >
+        <div class="header">
+          ✅ {{ checklist.title }} <button>Delete</button>
+        </div>
+        <div class="progress-bar"></div>
+
+        <ul class="todos">
+          <li v-for="todo in checklist.todos" :key="todo.id">
+            <input type="checkbox" id="todo.id" name="todo.id" />
+            <label for="todo.id"> {{todo.txt}} </label><br />
+          </li>
+        </ul>
+
       </li>
     </ul>
   </section>
