75c67c78b9a21af88d7b046a296c1600c8749ff0
Ifat BR <ifat.3d@gmail.com>
2021-03-17 15:45:15 +0200

    added popup

---
+++ frontend/src/components/pop-up.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pop-up">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot name="main">
+      </slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    components:{
+    }
+};
+</script>
+
+<style>
+</style>
