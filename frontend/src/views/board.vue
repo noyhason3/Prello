75737f4c00c1f4301dce9607090e4e5f805c51b3
SQUID13 <SQUADINIO13@gmail.com>
2021-03-18 16:30:37 +0200

    basic card layout

--- frontend/src/views/board.vue
+++ frontend/src/views/board.vue
@@ -1,7 +1,11 @@
 <template>
-  <section class="board">
-    <ul class="clean-list">
-      <li v-for="group in board.groups" :key="group.id">
+  <section class="board" ref="board">
+    <ul class="clean-list" ref="list">
+      <li
+        v-for="group in board.groups"
+        :key="group.id"
+        :style="`max-height:${maxHeight};`"
+      >
         <group :group="group" :boardId="board._id" />
       </li>
     </ul>
@@ -12,11 +16,19 @@
 <script>
 import group from "../cmps/board/group.vue";
 export default {
+  data() {
+    return {
+      maxHeight: "0px",
+    };
+  },
   computed: {
     board() {
       return this.$store.getters.currBoard;
     },
     groups() {},
+  },
+  mounted() {
+    this.maxHeight = this.$refs.list.clientHeight + "px";
   },
   components: { group },
 };
