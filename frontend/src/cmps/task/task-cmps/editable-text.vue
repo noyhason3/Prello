62668b98418a25ec69fd5c49be26196bb26e7189
SQUID13 <SQUADINIO13@gmail.com>
2021-03-18 10:34:45 +0200

    made taskTitle and taskDescription components more dynamic - names changed to editable-title and editable-text

--- frontend/src/cmps/task/task-cmps/editable-text.vue
+++ frontend/src/cmps/task/task-cmps/editable-text.vue
@@ -1,39 +1,48 @@
 <template>
   <section class="task-description">
-    <form v-if="taskDescriptionEdit" @submit.prevent="setDescription">
-      <textarea v-model="taskDescriptionEdit"></textarea>
+    <form v-if="isEditing" @submit.prevent="setDescription">
+      <textarea
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      ></textarea>
       <button>save</button>
       <button type="button" @click="closeDescription">X</button>
     </form>
-    <p v-else @click="editDescription">{{ taskDescriptionPreview }}</p>
+    <p v-else @click="editDescription">{{ value }}</p>
   </section>
 </template>
 
 <script>
 export default {
   props: {
-    currTaskDescription: String,
-    task: Object,
+    // currTaskDescription: String,
+    // task: Object,
+    value: String,
   },
   data() {
     return {
-      isEditDescription: false,
-      taskDescriptionPreview: this.currTaskDescription,
-      taskDescriptionEdit: null,
+      // isEditDescription: false,
+      // taskDescriptionPreview: this.currTaskDescription,
+      // taskDescriptionEdit: null,
+      isEditing: false,
     };
   },
   methods: {
     editDescription() {
-      this.taskDescriptionEdit = this.taskDescriptionPreview;
+      //this.taskDescriptionEdit = this.taskDescriptionPreview;
       // this.isEditDescription = true;
+
+      this.isEditing = true;
     },
     setDescription() {
-      this.taskDescriptionPreview = this.taskDescriptionEdit;
-      this.$emit("setDescription", this.taskDescriptionPreview);
-      this.closeDescription();
+      // this.taskDescriptionPreview = this.taskDescriptionEdit;
+      // this.$emit("setDescription", this.taskDescriptionPreview);
+      // this.closeDescription();
+      this.$emit("setDescription", this.value);
     },
     closeDescription() {
-      this.taskDescriptionEdit = null;
+      //this.taskDescriptionEdit = null;
+      this.isEditing = false;
     },
   },
 };
