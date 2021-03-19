514ee2cfdcd9cf4ccf22366cbdaa03064d9bbeb2
Ifat BR <ifat.3d@gmail.com>
2021-03-19 13:35:33 +0200

    pulling

--- frontend/src/cmps/task/task-cmps/task-attachment.vue
+++ frontend/src/cmps/task/task-cmps/task-attachment.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script>
-// import { uploadImg } from "@/services/img-upload.service.js";
 import attachment from "./attachment.vue";
 export default {
   props: {
@@ -21,26 +20,7 @@ export default {
         console.log('atts created');
     },
   methods: {
-    // dragOver(ev) {
-    //   console.log("drag over");
-    //   this.isDragOver = true;
-    // },
-    // handleFile(ev) {
-    //   console.log("handle");
-    //   let file;
-    //   if (ev.type === "change") file = ev.target.files[0];
-    //   else if (ev.type === "drop") file = ev.dataTransfer.files[0];
-    //   this.onUploadImg(file);
-    // },
-    // async onUploadImg(file) {
-    //   console.log("on upload");
-    //   this.isLoading = true;
-    //   this.isDragOver = false;
-    //   const res = await uploadImg(file);
-    //   this.$emit("save", res.url);
-    //   this.attachments.push(res.url);
-    //   this.isLoading = false;
-    // },
+
   },
   components: {
     attachment,
