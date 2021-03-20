250197a931da674b45b43ff8072b008eb6a380e2
Ifat BR <ifat.3d@gmail.com>
2021-03-20 17:30:39 +0200

    pulling

---
+++ frontend/src/cmps/common/member-preview.vue
@@ -0,0 +1,20 @@
+<template>
+      <a class="flex center">{{ member.initials }}</a>
+</template>
+
+<script>
+export default {
+    props:{
+        member:Object
+    },
+    created(){
+      // console.log('this.membersToShow:', this.member)
+    },
+    methods:{
+      toggleMemberDetails(){
+        this.$emit('toggle-member-details', {ev, member})
+      }
+    }
+};
+</script>
+
