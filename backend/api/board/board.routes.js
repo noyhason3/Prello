934297a84c19c601d98cdb0f1bdaee05ef16c542
noyhason3 <noyhason3@gmail.com>
2021-03-24 11:57:37 +0200

    sync

--- backend/api/board/board.routes.js
+++ backend/api/board/board.routes.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth.middleware')
-const {log} = require('../../middlewares/logger.middleware')
-const {getBoards, getBoard, addBoard, updateBoard, deleteBoard} = require('./board.controller')
-const router = express.Router()
+const express = require('express');
+const {
+  requireAuth,
+  requireAdmin,
+} = require('../../middlewares/requireAuth.middleware');
+const { log } = require('../../middlewares/logger.middleware');
+const {
+  getBoards,
+  getBoard,
+  addBoard,
+  updateBoard,
+  deleteBoard,
+} = require('./board.controller');
+const router = express.Router();
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
 router.get('/', getBoards);
+
 router.get('/:boardId', getBoard);
 
-router.post('/', addBoard); 
-// router.post('/', requireAuth, requireAdmin, addBoard); 
+router.post('/', addBoard);
+// router.post('/', requireAuth, requireAdmin, addBoard);
 
 router.put('/:boardId', updateBoard);
 // router.put('/:boardId', requireAuth, requireAdmin, updateBoard);
@@ -19,13 +29,4 @@ router.put('/:boardId', updateBoard);
 // router.delete('/:boardId' , deleteBoard);
 router.delete('/:boardId', requireAuth, requireAdmin, deleteBoard);
 
-
-
 module.exports = router;
-
-
-
-
-
-
-
--- backend/services/socket.service.js
+++ backend/services/socket.service.js
@@ -1,85 +1,81 @@
-
-
 const asyncLocalStorage = require('./als.service');
 const logger = require('./logger.service');
 
-var gIo = null
-var gSocketBySessionIdMap = {}
+var gIo = null;
+var gSocketBySessionIdMap = {};
 
 function connectSockets(http, session) {
-    gIo = require('socket.io')(http);
+  gIo = require('socket.io')(http);
 
-    const sharedSession = require('express-socket.io-session');
+  const sharedSession = require('express-socket.io-session');
 
-    gIo.use(sharedSession(session, {
-        autoSave: true
-    }));
-    gIo.on('connection', socket => {
-        console.log('New socket - socket.handshake.sessionID', socket.handshake.sessionID)
-        gSocketBySessionIdMap[socket.handshake.sessionID] = socket
-        console.log('session user:',socket.handshake?.session?.user);
-        // TODO: emitToUser feature - need to tested for CaJan21
-        // if (socket.handshake?.session?.user) socket.join(socket.handshake.session.user._id)
-        socket.on('disconnect', socket => {
-            console.log('Someone disconnected')
-            if (socket.handshake) {
-                gSocketBySessionIdMap[socket.handshake.sessionID] = null
-            }
-        })
-        socket.on('join-board', boardId => {
-            if (socket.currBoard === boardId) return;
-            if (socket.currBoard) {
-                socket.leave(socket.currBoard)
-            }
-            socket.join(boardId)
-            // logger.debug('Session ID is', socket.handshake.sessionID)
-            socket.currBoard = boardId
-        })
-        socket.on('save-board', board => {
-            socket.to(board._id).emit('board-update', board)
-        })
-        socket.on('join-task', taskId => {
-            socket.join(taskId)
-            socket.leave(taskId)
-        })
-        socket.on('leave-task', taskId => {
-            socket.leave(taskId)
-        })
-        socket.on('user-watch', userId => {
-            socket.join(userId)
-        })
-
+  gIo.use(
+    sharedSession(session, {
+      autoSave: true,
     })
+  );
+  gIo.on('connection', (socket) => {
+    console.log(
+      'New socket - socket.handshake.sessionID',
+      socket.handshake.sessionID
+    );
+    gSocketBySessionIdMap[socket.handshake.sessionID] = socket;
+    console.log('session user:', socket.handshake?.session?.user);
+    socket.on('disconnect', (socket) => {
+      console.log('Someone disconnected');
+      if (socket.handshake) {
+        gSocketBySessionIdMap[socket.handshake.sessionID] = null;
+      }
+    });
+    socket.on('join-board', (boardId) => {
+      if (socket.currBoard === boardId) return;
+      if (socket.currBoard) {
+        socket.leave(socket.currBoard);
+      }
+      socket.join(boardId);
+      socket.currBoard = boardId;
+    });
+    socket.on('save-board', (board) => {
+      socket.to(board._id).emit('board-update', board);
+    });
+    socket.on('join-task', (taskId) => {
+      socket.join(taskId);
+      socket.leave(taskId);
+    });
+    socket.on('leave-task', (taskId) => {
+      socket.leave(taskId);
+    });
+    socket.on('user-watch', (userId) => {
+      socket.join(userId);
+    });
+  });
 }
 
 function emitToAll({ type, data, room = null }) {
-    if (room) gIo.to(room).emit(type, data)
-    else gIo.emit(type, data)
+  if (room) gIo.to(room).emit(type, data);
+  else gIo.emit(type, data);
 }
 
-// TODO: Need to test emitToUser feature
 function emitToUser({ type, data, userId }) {
-    gIo.to(userId).emit(type, data)
+  gIo.to(userId).emit(type, data);
 }
 
-
-// Send to all sockets BUT not the current socket 
+// Send to all sockets BUT not the current socket
 function broadcast({ type, data, room = null }) {
-    const store = asyncLocalStorage.getStore()
-    const { sessionId } = store
-    if (!sessionId) return logger.debug('Shoudnt happen, no sessionId in asyncLocalStorage store')
-    const excludedSocket = gSocketBySessionIdMap[sessionId]
-    if (!excludedSocket) return logger.debug('Shouldnt happen, No socket in map')
-    if (room) excludedSocket.broadcast.to(room).emit(type, data)
-    else excludedSocket.broadcast.emit(type, data)
+  const store = asyncLocalStorage.getStore();
+  const { sessionId } = store;
+  if (!sessionId)
+    return logger.debug(
+      'Shoudnt happen, no sessionId in asyncLocalStorage store'
+    );
+  const excludedSocket = gSocketBySessionIdMap[sessionId];
+  if (!excludedSocket) return logger.debug('Shouldnt happen, No socket in map');
+  if (room) excludedSocket.broadcast.to(room).emit(type, data);
+  else excludedSocket.broadcast.emit(type, data);
 }
-
 
 module.exports = {
-    connectSockets,
-    emitToAll,
-    broadcast,
-}
-
-
-
+  connectSockets,
+  emitToAll,
+  broadcast,
+};
